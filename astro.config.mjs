ca749c032a77b01e09a258246d7649b1dbdc5eab
Laurent LESAGE <56911526+laurent-66@users.noreply.github.com>
2024-08-04 20:44:30 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://laurent-66.github.io',
+    site: 'https://laurent-66.github.io/portfolio-web',
   })
